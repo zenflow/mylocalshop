3942ca18c8d6e67f29095aeeb11d9ec30680750d
Matthew Francis Brunetti <zenflow87@gmail.com>
2020-04-20 19:59:55 -0400

    Install postgres client in hasura support app

--- hasura/support/lib/server.js
+++ hasura/support/lib/server.js
@@ -1,12 +1,31 @@
+const pg = require('pg')
 const express = require('express')
+
+{
+  async function doQuery (...args) {
+    const client = new pg.Client({ connectionString: process.env.DATABASE_URL })
+    client.connect()
+    const result = await client.query(...args)
+    client.end()
+    return result
+  }
+
+  const query = 'SELECT * FROM "public"."users"'
+  doQuery(query)
+    .then(({rows}) => {
+      console.log('query:', query)
+      console.log('rows:', rows)
+    })
+}
 
 const server = express()
 
-server.get('/auth-hook', (req, res) => {
-  // const token = req.get('Authorization')
-  // res.json({'X-Hasura-Role': 'user', 'X-Hasura-User-Id': '1'})
+server.get('/auth-hook', async (req, res) => {
   const role = req.get('Authorization') === 'yes' ? 'admin' : 'anonymous'
-  const vars = {'X-Hasura-Role': role}
+  const vars = {
+    'X-Hasura-Role': role,
+    // 'X-Hasura-User-Id': '1',
+  }
   console.log(vars)
   res.json(vars)
 })
