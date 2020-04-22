9ac842e5d7d3bb8cfc3d4e57025f337a277043a7
Matthew Francis Brunetti <zenflow87@gmail.com>
2020-04-22 02:20:10 -0400

    Remove postgres in hasura support app

--- hasura-support/lib/server.js
+++ hasura-support/lib/server.js
@@ -1,22 +1,4 @@
-const pg = require('pg')
 const express = require('express')
-
-{
-  async function doQuery (...args) {
-    const client = new pg.Client({ connectionString: process.env.DATABASE_URL })
-    client.connect()
-    const result = await client.query(...args)
-    client.end()
-    return result
-  }
-
-  const query = 'SELECT * FROM "public"."users"'
-  doQuery(query)
-    .then(({rows}) => {
-      console.log('query:', query)
-      console.log('rows:', rows)
-    })
-}
 
 const server = express()
 
